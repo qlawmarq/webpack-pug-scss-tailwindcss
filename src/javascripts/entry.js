2b973d96953a85d203e977a0524e3fc0cb97d092
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-07-26 00:09:34 +0900

    Improve UI

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -3,6 +3,7 @@ import '../css/base.scss';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 // HMRに失敗してもJSがリロードしてくれないので強制的にリロードを実行する
 if (module.hot) {
